30f1867f9e7610c8c32263a8d182ff2af94fd5a3
fengfeng <huangfeng191@163.com>
2021-05-13 14:01:06 +0800

    add lifecircle test

--- src/router/test.js
+++ src/router/test.js
@@ -8,6 +8,11 @@ export default[
    path: "/test/vuex",
    nme: "testvuex",
    component: () => import('../views/test/vuex/')
+},
+{
+   path: "/test/lifecircle",
+   nme: "testlifecircle",
+   component: () => import('../views/test/lifecircle/')
 }
   
 ]
---
+++ src/views/test/lifecircle/index.vue
@@ -0,0 +1,44 @@
+<template>
+   <div>
+      OhOOOOOO: ache {{regard}}
+   </div>
+</template>
+<script>
+export default {
+   data(){
+      return {
+            "regard":"I 'm shining!!!!"
+      }
+   },
+   beforeCreate:function(){
+      console.log(1);
+      debugger;
+   },
+    created:function(){
+      console.log(2);
+      debugger;
+   },
+   beforeMount:function(){
+      console.log(3);
+      debugger;
+   },
+   mounted:function(){
+      console.log(4);
+      debugger;
+      setTimeout(() => {
+         console.log("I'm destroying");
+         //  销毁的时 vue 对象，而不是 已经渲染的数据
+         this.$destroy();
+      }, 1000);
+   },
+   beforeDestroy:function(){
+      console.log(-1);
+      debugger;
+   },
+   destroyed:function(){
+      console.log("-0");
+      debugger;
+   }
+   
+}
+</script>
