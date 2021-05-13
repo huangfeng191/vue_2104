a2d6ecdc84026d1c84f9614dbb661f3424a2420c
fengfeng <huangfeng191@163.com>
2021-05-13 16:41:39 +0800

    a

---
+++ src/views/test/vuerouter/index.vue
@@ -0,0 +1,15 @@
+<template>
+   <div>
+      OhOOOOOO: ache {{regard}}
+   </div>
+</template>
+<script>
+export default{
+   data(){
+      return {
+             "regard":"Testing!!!!"
+      }
+   },
+
+}
+</script>
